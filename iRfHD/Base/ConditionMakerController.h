ae5497b81ae2b260ea221b746c8c4d3c6115f29e
88fantasy <88fantasy@gmail.com>
2013-03-22 10:08:35 +0800

    库存查询增加货品条件

---
+++ iRfHD/Base/ConditionMakerController.h
@@ -0,0 +1,13 @@
+//
+//  ConditionMakerController.h
+//  iRfHD
+//
+//  Created by pro on 13-3-20.
+//  Copyright (c) 2013年 rwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConditionMakerController : UITableViewController
+
+@end
